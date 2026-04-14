d11e1eeb19085a4b33b3c1cee1c0a3c7cf8dfbb3
Nil MALHOMME <malhomme.nil@gmail.com>
2026-04-14 20:08:04 +0200

    (main) fix: fixed a typo that was causing a compilation failure

--- src/main/java/io/github/ktestify/match/MatchResult.java
+++ src/main/java/io/github/ktestify/match/MatchResult.java
@@ -16,7 +16,7 @@
  * specific language governing permissions and limitations
  * under the License.
  */
-package io.github.ktestify.matchch;
+package io.github.ktestify.match;
 
 import lombok.Value;
 
